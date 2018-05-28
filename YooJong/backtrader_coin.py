911a1c76c5c9ecb07342a5f68f56c72fbcf81f41
YooJong Lee <olrai011@gmail.com>
2018-05-28 18:41:27 +0900

    update Strategy

---
+++ YooJong/backtrader_coin.py
@@ -0,0 +1,80 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
+import pandas as pd
+import datetime
+
+# Import the backtrader platform
+import backtrader as bt
+import backtrader.indicators as btind
+import backtrader.feeds as btfeeds
+from mydatafeed import MyDataFeed
+
+# import extensions
+from extensions.observer.ob_acctvalue import AcctValue
+from extensions.convert_raw_data import convert_raw_data_from_KI
+
+# import strategies
+from Strategies.HighRateLong import HighRateLong
+
+
+'''
+    1. cerebro를 만들고 데이터를 넣어준다.
+    2. Broker -> Observer -> Strategy -> Analyzer -> Writer 순으로 정의
+'''
+if __name__ == '__main__' :
+
+    cerebro = bt.Cerebro(stdstats=False)
+    datapath = './resources/etc.csv'
+    dataframe = convert_raw_data_from_KI(datapath)
+
+    data = bt.feeds.PandasData(dataname=dataframe,
+                               datetime=0
+                               )
+    # cerebro.adddata(data)
+    cerebro.resampledata(dataname=data,
+                         timeframe = bt.TimeFrame.Days)
+
+    ''' -------------------------------------------------------------------  '''
+    # Broker setting
+    start_value = 10000000
+    cerebro.broker.setcash(start_value)
+    cerebro.broker.setcommission(commission=0.005)
+    cerebro.addsizer(bt.sizers.PercentSizer)
+
+    ''' -------------------------------------------------------------------  '''
+    # Observer Setting
+    cerebro.addobserver(bt.observers.BuySell)
+    cerebro.addobserver(AcctValue)
+
+    ''' -------------------------------------------------------------------  '''
+    # Strategy Setting
+    cerebro.addstrategy(HighRateLong)
+
+
+    ''' -------------------------------------------------------------------  '''
+    # Analyzer Setting
+    # cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name="ta")
+    # cerebro.addanalyzer(bt.analyzers.SQN, _name="sqn")
+
+
+    ''' -------------------------------------------------------------------  '''
+    # Writer Setting
+    # cerebro.addwriter(bt.WriterFile, csv=True)
+
+
+    ''' -------------------------------------------------------------------  '''
+    # Run cerebro
+    cerebro.run()
+
+
+    # print the analyzers
+    # printTradeAnalysis(firstStrat.analyzers.ta.get_analysis())
+    # printSQN(firstStrat.analyzers.sqn.get_analysis())
+
+    ''' -------------------------------------------------------------------  '''
+    # Plot the result
+    cerebro.plot(style='candle')
--- YooJong/extensions/convert_raw_data.py
+++ YooJong/extensions/convert_raw_data.py
@@ -6,15 +6,18 @@ from datetime import datetime
 
 
 
-def convert_raw_dat_from_KI(raw_data_path):
+def convert_raw_data_from_KI(raw_data_path):
+    print("------    Converting Raw Data    ------")
     data = pd.read_csv(raw_data_path)
     data['timestamp'] = data['timestamp'].map(lambda x : datetime.fromtimestamp(int( x / 1000)))
-    data['date'] = data['timestamp'].map(lambda x : list(str(x).split(' '))[0])
-    data['time'] = data['timestamp'].map(lambda x : list(str(x).split(' '))[1])
+    data['datetime'] = data['timestamp']
+    # data['date'] = data['timestamp'].map(lambda x : list(str(x).split(' '))[0])
+    # data['time'] = data['timestamp'].map(lambda x : list(str(x).split(' '))[1])
     data.drop(['timestamp'], axis=1, inplace=True)
     data.drop(['accPrice', 'candleDateTime', 'candleDateTimeKst'], axis=1, inplace=True)
-    data = data[['date', 'time', 'volume', 'high', 'low', 'open', 'close']]
-    data.to_csv('BTC_to_Backtrader.csv', index=False, sep=',', encoding='utf-8' )
+    data = data[['datetime', 'volume', 'high', 'low', 'open', 'close']]
+    # data = data[['date', 'time', 'volume', 'high', 'low', 'open', 'close']]
+    # data.to_csv('BTC_to_Backtrader.csv', index=False, sep=',', encoding='utf-8' )
     return data
 
 def convert_raw_data_from_csv(raw_data_path):
